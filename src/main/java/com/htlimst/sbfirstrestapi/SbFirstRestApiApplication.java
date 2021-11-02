5b0c096c140ef1d19d832c411f6d4272eeb31a24
meilinger7 <julianmeilinger07@gmail.com>
2021-11-02 18:21:05 +0100

    initial commit

---
+++ src/main/java/com/htlimst/sbfirstrestapi/SbFirstRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.htlimst.sbfirstrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbFirstRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbFirstRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/htlimst/sbfirstrestapi/SbFirstRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htlimst.sbfirstrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbFirstRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
