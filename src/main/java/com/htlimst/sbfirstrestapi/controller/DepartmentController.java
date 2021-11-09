28945a4019c56e84e7a6c6b86b9ece09d87ae872
meilinger7 <julianmeilinger07@gmail.com>
2021-11-09 22:31:19 +0100

    ErrorHandeling commit

--- src/main/java/com/htlimst/sbfirstrestapi/controller/DepartmentController.java
+++ src/main/java/com/htlimst/sbfirstrestapi/controller/DepartmentController.java
@@ -1,10 +1,14 @@
 package com.htlimst.sbfirstrestapi.controller;
 
 import com.htlimst.sbfirstrestapi.entity.Department;
+import com.htlimst.sbfirstrestapi.error.DepartmentNotFoundException;
 import com.htlimst.sbfirstrestapi.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -13,19 +17,24 @@ public class DepartmentController {
     @Autowired
     private  DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
+
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department){
+    public Department saveDepartment(@Valid @RequestBody Department department){
+        LOGGER.info("Inside the saveDepartment of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList(){
+        LOGGER.info("Inside the fetchDepartmentList of DepartmentController");
         return departmentService.fetchDepartmentList();
 
     }
 
     @GetMapping("/departments/{id}")
-    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) throws DepartmentNotFoundException {
         return departmentService.fetchDepartmentById(departmentId);
     }
 
@@ -39,5 +48,10 @@ public class DepartmentController {
     public Department updateDepartment(@PathVariable("id") Long departmentId,
                                        @RequestBody Department department){
         return departmentService.updateDepartment(departmentId, department);
+    }
+
+    @GetMapping("/departments/name/{name}")
+    public Department fetchDepartmentByName(@PathVariable("name") String departmentName){
+        return departmentService.fetchDepartmentByName(departmentName);
     }
 }
--- src/main/java/com/htlimst/sbfirstrestapi/service/DepartmentServiceImpl.java
+++ src/main/java/com/htlimst/sbfirstrestapi/service/DepartmentServiceImpl.java
@@ -1,12 +1,14 @@
 package com.htlimst.sbfirstrestapi.service;
 
 import com.htlimst.sbfirstrestapi.entity.Department;
+import com.htlimst.sbfirstrestapi.error.DepartmentNotFoundException;
 import com.htlimst.sbfirstrestapi.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
@@ -25,8 +27,13 @@ public class DepartmentServiceImpl implements DepartmentService {
     }
 
     @Override
-    public Department fetchDepartmentById(Long departmentId) {
-        return departmentRepository.findById(departmentId).get();
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException {
+        Optional<Department> department = departmentRepository.findById(departmentId);
+
+        if (!department.isPresent()){
+            throw new DepartmentNotFoundException("Department not found");
+        }
+        return department.get();
     }
 
     @Override
@@ -53,5 +60,10 @@ public class DepartmentServiceImpl implements DepartmentService {
         }
 
         return departmentRepository.save(depDB);
+    }
+
+    @Override
+    public Department fetchDepartmentByName(String departmentName) {
+        return departmentRepository.findByDepartmentNameIgnoreCase(departmentName);
     }
 }
